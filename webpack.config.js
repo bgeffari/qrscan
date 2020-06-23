03a34d912b32c6b10a8e08ef64d9d89d3f33ff42
Bader Algeffari <bgeffari@gmail.com>
2020-06-23 09:10:50 +0300

    back to 0.0.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   devServer: {
     contentBase: __dirname + '/app',
     disableHostCheck: true,
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 3000,
     public: 'qcc.evntez.com:8080'
   },
